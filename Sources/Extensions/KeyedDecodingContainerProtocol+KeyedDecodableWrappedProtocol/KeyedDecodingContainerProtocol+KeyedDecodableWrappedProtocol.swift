0a14a5a7065136fed17a60ea9b8e3b9a61772668
Misha Nikanorov <fobozsb@gmail.com>
2021-06-25 19:23:07 -0700

    develop: 2 tests; minor naming and visibility changes

--- Sources/Extensions/KeyedDecodingContainerProtocol+KeyedDecodableWrappedProtocol/KeyedDecodingContainerProtocol+KeyedDecodableWrappedProtocol.swift
+++ Sources/Extensions/KeyedDecodingContainerProtocol+KeyedDecodableWrappedProtocol/KeyedDecodingContainerProtocol+KeyedDecodableWrappedProtocol.swift
@@ -10,19 +10,19 @@ import Foundation
 public extension KeyedDecodingContainerProtocol {
   func decode<T:KeyedDecodableWrappedProtocol>(wrappedType: String.Type, forKey key: Self.Key) throws -> T {
     let wrapped = try self.decode(wrappedType, forKey: key)
-    guard let decimal = T(wrapped: wrapped) else {
+    guard let value = T(wrapped: wrapped) else {
       throw DecodingError.dataCorruptedError(forKey: key, in: self, debugDescription: "Value: \(wrapped)")
     }
-    return decimal
+    return value
   }
   
   func decodeIfPresent<T:KeyedDecodableWrappedProtocol>(wrappedType: String.Type, forKey key: Self.Key) throws -> T? {
     guard let wrapped = try self.decodeIfPresent(wrappedType, forKey: key) else {
       return nil
     }
-    guard let decimal = T(wrapped: wrapped) else {
+    guard let value = T(wrapped: wrapped) else {
       throw DecodingError.dataCorruptedError(forKey: key, in: self, debugDescription: "Value: \(wrapped)")
     }
-    return decimal
+    return value
   }
 }
--- Tests/MEWextensionsTests/MEWextensionsTests.swift
+++ Tests/MEWextensionsTests/MEWextensionsTests.swift
@@ -1,11 +1,48 @@
-    import XCTest
-    @testable import MEWextensions
+import XCTest
+@testable import MEWextensions
 
-    final class MEWextensionsTests: XCTestCase {
-        func testExample() {
-            // This is an example of a functional test case.
-            // Use XCTAssert and related functions to verify your tests produce the correct
-            // results.
-            XCTAssertEqual(MEWextensions().text, "Hello, World!")
-        }
+final class MEWextensionsTests: XCTestCase {
+  
+  func testKeyedDecodingContainerProtocolFailure() {
+    struct TestStruct: Decodable {
+      // MARK: - Coding Keys
+      private enum CodingKeys: CodingKey {
+        case value
+      }
+      
+      let value: Decimal
+      
+      init(from decoder: Decoder) throws {
+        let container         = try decoder.container(keyedBy: CodingKeys.self)
+        let value: Decimal    = try container.decode(wrappedType: String.self, forKey: .value)
+        self.value = value
+      }
     }
+    
+    let data = #"{"value":"abc"}"#.data(using: .utf8)!
+    
+    let decoder = JSONDecoder()
+    XCTAssertThrowsError(try decoder.decode(TestStruct.self, from: data))
+  }
+  
+  func testKeyedEncodingContainerProtocolFailure() {
+    struct TestStruct: Encodable {
+      // MARK: - Coding Keys
+      private enum CodingKeys: CodingKey {
+        case value
+      }
+      
+      let value: Decimal
+      
+      func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(self.value, wrappedType: Bool.self, forKey: .value)
+      }
+    }
+    
+    let encoder = JSONEncoder()
+    let testStructNotSupported = TestStruct(value: .greatestFiniteMagnitude)
+    XCTAssertThrowsError(try encoder.encode(testStructNotSupported))
+  }
+  
+}
