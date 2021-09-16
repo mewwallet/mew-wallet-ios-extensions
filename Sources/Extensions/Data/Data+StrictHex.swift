5ec65203418f3cec1e5f21711c33d5cde0142669
Misha Nikanorov <fobozsb@gmail.com>
2021-09-16 15:59:41 -0700

    feature/0x: Make 0x valid for Data

--- Sources/Extensions/Data/Data+StrictHex.swift
+++ Sources/Extensions/Data/Data+StrictHex.swift
@@ -9,8 +9,12 @@ import Foundation
 
 extension Data: StrictHexProtocol {
   public init?(strictHex hex: String) {
+    guard hex.hasPrefix("0x") else { return nil }
     let bytes = Array<UInt8>(hex: hex)
-    guard !bytes.isEmpty else { return nil }
+    guard !bytes.isEmpty else {
+      self = Data()
+      return
+    }
     
     self.init(bytes)
   }
