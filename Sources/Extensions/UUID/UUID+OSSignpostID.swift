47dcd12c05a925109e22d176a4fe1177c08ac8c0
Misha Nikanorov <fobozsb@gmail.com>
2021-10-21 00:41:14 -0700

    feature/extra_extensions: Added UUID+OSSignpostID

---
+++ Sources/Extensions/UUID/UUID+OSSignpostID.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Mikhail Nikanorov on 10/21/21.
+//
+
+import Foundation
+import OSLog
+
+extension UUID {
+  public var signpostID: OSSignpostID {
+    return OSSignpostID(self.uint64uuid)
+  }
+}
