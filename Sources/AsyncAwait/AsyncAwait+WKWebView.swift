4c2a65b41cd8ba3366bc2ef2dc34c22201e1cbcf
MityaPolianskii <liweba@icloud.com>
2022-05-17 22:58:14 +0400

    changed to a better solution

--- Sources/AsyncAwait/AsyncAwait+WKWebView.swift
+++ Sources/AsyncAwait/AsyncAwait+WKWebView.swift
@@ -9,17 +9,15 @@ import WebKit
 
 // Can it live here?
 extension WKWebView {
-  @discardableResult
-  public func evaluateJavaScriptAsync(_ str: String) async throws -> Any? {
+  @MainActor
+  public func safeEvaluateJavaScript(_ javaScriptString: String) async throws -> Any? {
     return try await withCheckedThrowingContinuation { continuation in
-      DispatchQueue.main.async {
-        self.evaluateJavaScript(str) { data, error in
-          if error != nil {
-            continuation.resume(returning: nil)
-          } else {
-            continuation.resume(returning: data)
-          }
+      self.evaluateJavaScript(javaScriptString) { result, error in
+        if let error = error {
+          continuation.resume(throwing: error)
+          return
         }
+        continuation.resume(returning: result)
       }
     }
   }
